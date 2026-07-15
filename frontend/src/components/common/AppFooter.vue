1aec5ff18c6ba2f0470b06f2129627d329d24205
moonsyu <anstn0104@naver.com>
2026-07-15 15:53:20 +0900

    푸터 메뉴와 개발자 정보 연동

--- frontend/src/components/common/AppFooter.vue
+++ frontend/src/components/common/AppFooter.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const developers = (import.meta.env.VITE_DEVELOPERS || "")
+  .split(",")
+  .map((name) => name.trim())
+  .filter(Boolean);
+</script>
+
 <template>
   <footer class="app-footer">
     <div class="container app-footer__inner story-footer">
@@ -5,9 +14,28 @@
         <strong>Seoul ITDA</strong>
         <p>서울의 장소와 축제, 사람의 이야기를 다정하게 잇습니다.</p>
       </div>
-      <div><b>메뉴</b><p>홈<br />축제 캘린더<br />커뮤니티</p></div>
-      <div><b>서비스</b><p>서울 명소<br />문화 행사<br />여행 후기</p></div>
-      <div><b>Contact</b><p>Seoul, Republic of Korea<br />hello@seoul-itda.kr</p></div>
+
+      <nav class="story-footer__group" aria-label="푸터 메뉴">
+        <b>메뉴</b>
+        <ul class="story-footer__links">
+          <li><RouterLink to="/">홈</RouterLink></li>
+          <li><RouterLink to="/festivals">축제 캘린더</RouterLink></li>
+          <li><RouterLink to="/posts">커뮤니티</RouterLink></li>
+        </ul>
+      </nav>
+
+      <div class="story-footer__group">
+        <b>Developer</b>
+        <ul v-if="developers.length" class="story-footer__developers" aria-label="개발자 목록">
+          <li v-for="developer in developers" :key="developer">{{ developer }}</li>
+        </ul>
+        <p v-else>개발자 정보가 등록되지 않았습니다.</p>
+      </div>
+
+      <div class="story-footer__group">
+        <b>Contact</b>
+        <p>Seoul, Republic of Korea<br />hello@seoul-itda.kr</p>
+      </div>
     </div>
   </footer>
 </template>
